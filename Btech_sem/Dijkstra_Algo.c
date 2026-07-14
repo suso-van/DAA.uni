69559caeb4b894495a8c780ee54abb93c3fa884d
susovan <chatterjeesusovan2004@gmail.com>
2026-07-15 03:06:32 +0530

    Add Dijkstra's algorithm implementation
    Implement Dijkstra's algorithm to find shortest paths from a source vertex to all other vertices in a graph using a cost matrix.

--- Btech_sem/Dijkstra_Algo.c
+++ Btech_sem/Dijkstra_Algo.c
@@ -1,1 +1,59 @@
+#include<stdio.h>
 
+#define INF 9999
+
+int main()
+{
+    int n,i,j,start;
+    int cost[10][10];
+    int dist[10],visited[10]={0};
+
+    printf("Enter vertices: ");
+    scanf("%d",&n);
+
+    printf("Enter cost matrix:\n");
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&cost[i][j]);
+
+    printf("Enter source: ");
+    scanf("%d",&start);
+
+    for(i=0;i<n;i++)
+        dist[i]=cost[start][i];
+
+    visited[start]=1;
+    dist[start]=0;
+
+    for(i=1;i<n;i++)
+    {
+        int min=INF,u;
+
+        for(j=0;j<n;j++)
+        {
+            if(!visited[j] && dist[j]<min)
+            {
+                min=dist[j];
+                u=j;
+            }
+        }
+
+        visited[u]=1;
+
+        int k;
+
+        for(k=0;k<n;k++)
+        {
+            if(!visited[k] && dist[u]+cost[u][k]<dist[k])
+                dist[k]=dist[u]+cost[u][k];
+        }
+    }
+
+    printf("Shortest distances:\n");
+
+    for(i=0;i<n;i++)
+        printf("%d -> %d = %d\n",start,i,dist[i]);
+
+    return 0;
+}
